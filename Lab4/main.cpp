d2c04cc3ddd30924fce586cbdb3c09b04c00082b
Pascal Attwenger <pascal.attwenger@gmail.com>
2014-10-15 21:02:06 +0100

    Initial Commit

---
+++ Lab4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab4
+//
+//  Created by Pascal Attwenger on 15/10/2014.
+//  Copyright (c) 2014 Pascal Attwenger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
